a129e095ba2241148ca090b8efa36b3985292b23
Familia Porcel <brandon7.7porcel@gmail.com>
2020-10-28 19:33:07 -0300

    agregue el navegador donde estas

--- src/js/modules/sound_details.js
+++ src/js/modules/sound_details.js
@@ -3,6 +3,7 @@ export function soundDetails() {
 	const $tracklist = d.getElementById('track-list');
 	const $progress = d.querySelector('.progress');
 	const $likedIcon = d.querySelector('.control__btn-liked');
+	const $device = d.querySelector('.device__browser');
 	let likedIconTester = false;
 	d.querySelector('.control__btn-trackplaying').addEventListener(
 		'click',
@@ -27,4 +28,23 @@ export function soundDetails() {
 			likedIconTester = false;
 		}
 	});
+	const isBrowser = {
+		chromeee: () => navigator.userAgent.match(/chrome/i),
+		safarai: () => navigator.userAgent.match(/safarai/i),
+		firefoxSAPEEEE: () => navigator.userAgent.match(/firefox/i),
+		opera: () => navigator.userAgent.match(/opera|opera mini/i),
+		ie: () => navigator.userAgent.match(/msie|iemobile/i),
+		edge: () => navigator.userAgent.match(/edge/i),
+		any: function () {
+			return (
+				this.ie() ||
+				this.edge() ||
+				this.chromeee() ||
+				this.safarai() ||
+				this.firefoxSAPEEEE() ||
+				this.opera()
+			);
+		},
+	};
+	$device.innerHTML = `${isBrowser.any()}`;
 }
