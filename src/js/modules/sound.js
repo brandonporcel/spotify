97abffaf21c120f22603a3e72209a6ac815d9cb4
Familia Porcel <brandon7.7porcel@gmail.com>
2020-10-28 12:04:10 -0300

    queiro arreglar q cuando haga click en un option se reproduzca

--- src/js/modules/sound.js
+++ src/js/modules/sound.js
@@ -1,53 +1,32 @@
 export function sound() {
 	const d = document;
-	const $playBtn = d.getElementById('play-btn');
+	const $nameSong = d.getElementById('info-name');
 	// input file
 	const $btnFiles = d.getElementById('add-files');
+	const $nextBtn = d.getElementById('next-btn');
+	const $prevBtn = d.getElementById('prev-btn');
+	const $playBtn = d.getElementById('play-btn');
+	const $pauseBtn = d.getElementById('pause-btn');
 	// etiqueta audio
 	const $audioAdded = d.getElementById('audio-added');
-	const $tracklist = d.querySelector('.list');
+	const $tracklist = d.getElementById('track-list');
 	const $progress = d.querySelector('.progress');
 	const $playbackProgressText = d.querySelector('.playback__progress-text');
 	const $playbackDurationText = d.querySelector('.playback__duration');
-	// donde sale el titulo de la cancion que suena
-	const trackPlayingNow = d.getElementById('playing-now');
 	// donde salen los nombres de las canciones que se pusierpn
 	const $repeatSongBtn = d.getElementById('btn-auto-track');
+	const $volumeInput = d.getElementById('volume-bar');
 	const urlTracks = [];
+	let listaCanciones = [];
 	const audioPositionTracklist = [];
 	let contadorPositionTracklist = 0;
 	let audioPorciento = 0;
 	let nextTrackActive = false;
-	const obtenerMusica = () => {
-		// segun la cantidad de archivos seleccionados
-		for (let iterator = 0; iterator < $btnFiles.files.length; iterator += 1) {
-			// obtengo la url del item(audio)
-			// https://stackoverflow.com/questions/30864573/what-is-a-blob-url-and-why-it-is-used
-			// btn.files(obtengo los archivos)[iterator](en cada posicion,o sea, todos ya que es un for)
-			const objetoURL = URL.createObjectURL($btnFiles.files[iterator]);
-			// pongo las url en modo de objeto en este array
-			urlTracks.push({
-				archivo: $btnFiles.files[iterator],
-				url: objetoURL,
-			});
-			// le pongo que canciones selecciono, es += para que no se quede solo con el ultimo item
-			$tracklist.innerHTML += `<b class"name-track">${urlTracks[iterator].archivo.name}</b>`;
-			// guardo su posicion en otro array
-			audioPositionTracklist.push({
-				musica: $btnFiles.files[iterator],
-				posicion: contadorPositionTracklist,
-			});
-			contadorPositionTracklist += 1;
-		}
-		// lo regreso a 0 para que siempre empiece en this number
-		contadorPositionTracklist = 0;
-		// ahora la etiqueta tiene el src y ahora puedo reproducirlo
-		$audioAdded.src = urlTracks[contadorPositionTracklist].url;
-	};
 
 	const play = () => {
 		$audioAdded.play();
-		trackPlayingNow.innerText = `escuchando ${urlTracks[contadorPositionTracklist].archivo.name}`;
+		$nameSong.classList.add('active');
+		$nameSong.innerText = `${urlTracks[contadorPositionTracklist].archivo.name}`;
 		setInterval(() => {
 			let Totalsegundos = parseInt($audioAdded.currentTime, 10);
 			Totalsegundos %= 3600;
@@ -72,6 +51,60 @@ export function sound() {
 			)}%`;
 		}, 400);
 	};
+	// cuando haga click en una cancion del select(html)
+	const musicaDeTracklist = () => {
+		console.log('musica del traklsisisisisi');
+		for (let iterator = 0; iterator < listaCanciones.length; iterator += 1) {
+			console.log('kujstaa cabnciones');
+			listaCanciones.forEach((el) => {
+				console.log('s');
+				el.addEventListener('click', (e) => {
+					console.log('clickkkk dn item');
+					if (e.target.innerHTML === urlTracks[iterator].archivo.name) {
+						$audioAdded.src = urlTracks[iterator].url;
+						$nameSong.innerText = `${urlTracks[iterator].archivo.name}`;
+						// por esta linea eslint me dice que no cumplo un par de reglas.i dont care
+						contadorPositionTracklist = iterator;
+					}
+					play();
+				});
+			});
+		}
+	};
+	const obtenerMusica = () => {
+		// segun la cantidad de archivos seleccionados
+		for (let iterator = 0; iterator < $btnFiles.files.length; iterator += 1) {
+			// obtengo la url del item(audio)
+			// https://stackoverflow.com/questions/30864573/what-is-a-blob-url-and-why-it-is-used
+			// btn.files(obtengo los archivos)[iterator](en cada posicion,o sea, todos ya que es un for)
+			const objetoURL = URL.createObjectURL($btnFiles.files[iterator]);
+			// pongo las url en modo de objeto en este array
+			urlTracks.push({
+				archivo: $btnFiles.files[iterator],
+				url: objetoURL,
+			});
+			audioPositionTracklist.push({
+				musica: $btnFiles.files[iterator],
+				posicion: contadorPositionTracklist,
+			});
+			// le pongo que canciones selecciono, es += para que no se quede solo con el ultimo item
+			contadorPositionTracklist += 1;
+			$tracklist.classList.add('active');
+			$tracklist.innerHTML += `
+			<option class="tracklist__name" >${urlTracks[iterator].archivo.name}</option>
+			`;
+		}
+		// cuando tenga agregue audios,ahi voya  poder elegir canciones del select
+		listaCanciones = d.querySelectorAll('.tracklist__name');
+		musicaDeTracklist();
+		// lo regreso a 0 para que siempre empiece en this number
+		contadorPositionTracklist = 0;
+		// ahora la etiqueta tiene el src y ahora puedo reproducirlo
+		$audioAdded.src = urlTracks[contadorPositionTracklist].url;
+	};
+	const pause = () => {
+		$audioAdded.pause();
+	};
 	// Automatizar musica siguiente
 	const musicaSiguiente = () => {
 		contadorPositionTracklist += 1;
@@ -81,6 +114,52 @@ export function sound() {
 		$audioAdded.src = urlTracks[contadorPositionTracklist].url;
 		play();
 	};
+	// manejador del volumen bruh
+	const controlAudio = (e) => {
+		$audioAdded.volume = e.target.value;
+		if ($audioAdded.volume === 0) {
+			console.log('cerro');
+			d.querySelector('.control__btn-volume').classList.remove('fa-volume-up');
+			d.querySelector('.control__btn-volume').classList.remove(
+				'fa-volume-down'
+			);
+			d.querySelector('.control__btn-volume').classList.add('fa-volume-mute');
+		}
+		if ($audioAdded.volume <= 0.5 && $audioAdded.volume > 0) {
+			d.querySelector('.control__btn-volume').classList.remove(
+				'fa-volume-mute'
+			);
+			d.querySelector('.control__btn-volume').classList.remove('fa-volume-up');
+			d.querySelector('.control__btn-volume').classList.add('fa-volume-down');
+		}
+		if ($audioAdded.volume > 0.5) {
+			console.log('Mayor a 5');
+			d.querySelector('.control__btn-volume').classList.remove(
+				'fa-volume-down'
+			);
+			d.querySelector('.control__btn-volume').classList.remove(
+				'fa-volume-mute'
+			);
+			d.querySelector('.control__btn-volume').classList.add('fa-volume-up');
+		}
+	};
+	const cancionSiguiente = () => {
+		contadorPositionTracklist += 1;
+		if (contadorPositionTracklist > urlTracks.length - 1) {
+			contadorPositionTracklist = 0;
+		}
+		$audioAdded.src = urlTracks[contadorPositionTracklist].url;
+		play();
+	};
+	const cancionAnterior = () => {
+		contadorPositionTracklist -= 1;
+		if (contadorPositionTracklist < 0) {
+			contadorPositionTracklist = urlTracks.length - 1;
+		}
+		$audioAdded.src = urlTracks[contadorPositionTracklist].url;
+		play();
+	};
+	// para verificar ciuando termina un tema. lo pongo aca y no en una funcoion para qu efuncione
 	setInterval(() => {
 		if (audioPorciento === 100) {
 			musicaSiguiente();
@@ -94,16 +173,20 @@ export function sound() {
 	const finalMusica = () => {
 		if (nextTrackActive === false) {
 			$audioAdded.setAttribute('loop', '');
-			$repeatSongBtn.innerText = 'AutoTrack-on';
+			d.querySelector('.control-btn-repeat').classList.add('active');
 			nextTrackActive = true;
 		} else {
 			nextTrackActive = false;
+			d.querySelector('.control-btn-repeat').classList.remove('active');
 			$audioAdded.removeAttribute('loop');
-			$repeatSongBtn.innerText = 'AutoTrack-off';
 		}
 	};
-	$btnFiles.addEventListener('change', obtenerMusica);
 	$playBtn.addEventListener('click', play);
+	$btnFiles.addEventListener('change', obtenerMusica);
+	$pauseBtn.addEventListener('click', pause);
 	$repeatSongBtn.addEventListener('click', finalMusica);
+	$volumeInput.addEventListener('change', controlAudio);
+	$nextBtn.addEventListener('click', cancionSiguiente);
+	$prevBtn.addEventListener('click', cancionAnterior);
 	musicaSiguiente();
 }
