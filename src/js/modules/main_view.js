2c1d29876eb7ed42c1a36e221ed6341e8e62f1c8
Familia Porcel <brandon7.7porcel@gmail.com>
2020-10-31 20:52:28 -0300

    done,era una boludez

--- src/js/modules/main_view.js
+++ src/js/modules/main_view.js
@@ -1,6 +1,27 @@
 const d = document;
 export function mainView() {
 	const $ctn = d.querySelector('.mainView-ctn');
+	const isBrowser = {
+		chromeee: () => navigator.userAgent.match(/chrome/i),
+		safarai: () => navigator.userAgent.match(/safarai/i),
+		firefoxSAPEEEE: () => navigator.userAgent.match(/firefox/i),
+		opera: () => navigator.userAgent.match(/opera|opera mini/i),
+		ie: () => navigator.userAgent.match(/msie|iemobile/i),
+		edge: () => navigator.userAgent.match(/edge/i),
+		any: function () {
+			return (
+				this.ie() ||
+				this.edge() ||
+				this.chromeee() ||
+				this.safarai() ||
+				this.firefoxSAPEEEE() ||
+				this.opera()
+			);
+		},
+	};
+	if (isBrowser.chromeee()) {
+		document.querySelector('.mainView-ctn').style.height = '480px';
+	}
 	$ctn.addEventListener('scroll', () => {
 		const topDistance = $ctn.scrollTop;
 		if (topDistance > 100) {
