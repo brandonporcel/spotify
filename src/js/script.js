4e20e7e09a1a257ce3ceb2b89b56ed580ac5df9c
Familia Porcel <brandon7.7porcel@gmail.com>
2020-10-31 18:02:17 -0300

    me acabo de dar cuenta q el navIzq es diferente en chrome y mozilla, lo wa  arreglar...

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,9 @@
 import { soundDetails } from './modules/sound_details.js';
 import { sound } from './modules/sound.js';
+import { topBar } from './modules/top_bar.js';
 
 document.addEventListener('DOMContentLoaded', () => {
 	soundDetails();
 	sound();
+	topBar();
 });
